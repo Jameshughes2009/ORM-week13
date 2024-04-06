32c138096671c1c2c69bcba1e3c9870d3c2e6ecf
James Hughes <jameshughes2009@hotmail.com>
2024-04-06 12:21:13 -0400

    more testing

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,9 +3,14 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get('/', async (req, res) => {
+  try{
+    const categories = await Category.findAll({ include: [{model: Product}] });
+    res.status(200).json(categories);
+  } catch(err) {
+    
+  }
+  // find all categories as well as to be sure to include its associated Products
 });
 
 router.get('/:id', (req, res) => {
