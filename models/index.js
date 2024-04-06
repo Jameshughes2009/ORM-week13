6525e02451f39ad0d292b9bf7ae634acb06bc066
James Hughes <jameshughes2009@hotmail.com>
2024-04-06 13:05:18 -0400

    adding index and linking variables

--- models/index.js
+++ models/index.js
@@ -5,10 +5,21 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'categroy_id',
+})
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+});
+
 
 // Tags belongToMany Products (through ProductTag)
 
