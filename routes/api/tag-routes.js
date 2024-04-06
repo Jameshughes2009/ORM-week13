6158af732ed0575b44e84cfad54c4fca492cc144
James Hughes <jameshughes2009@hotmail.com>
2024-04-06 12:27:56 -0400

    trying to fix error

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,11 +1,22 @@
 const router = require('express').Router();
-const { Tag, Product, ProductTag } = require('../../models');
+const { Tag, Product} = require('../../models');
 
 // The `/api/tags` endpoint
 
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+});
+// Get all tags
+router.get("/", async (req, res) => {
+  try {
+    const tagData = await Tag.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json({ message: "Tags not found!" });
+  }
 });
 
 router.get('/:id', (req, res) => {
