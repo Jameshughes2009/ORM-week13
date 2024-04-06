8d9647bf3227ddf9ecf650633ec1102bf32024e9
James Hughes <jameshughes2009@hotmail.com>
2024-04-06 11:49:24 -0400

    more seeding info for porduct and tags models

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,12 +1,33 @@
 const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
+const Product = require('./Product');
 
 class ProductTag extends Model {}
 
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "tag",//
+        key: "id",
+      }
+    },
+    produc_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "product",
+        key: "id",
+      }
+    }
   },
   {
     sequelize,
