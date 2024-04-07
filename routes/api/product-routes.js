48f6ef823775081b92968a1fe68985407bf580a5
James Hughes <jameshughes2009@hotmail.com>
2024-04-07 08:21:11 -0400

    get all products function

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,10 +4,18 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
-});
+// router.get('/', (req, res) => {
+//   // find all products
+//   // be sure to include its associated Category and Tag data
+// });
+router.get("/", async (req, res) => {
+  try {
+    const products = await Product.findAll({
+      include: [{model: Category}, {model: Tag}],
+    });
+    res.status(200).json(products);
+  } catch (err) {}
+})
 
 // get one product
 router.get('/:id', (req, res) => {
