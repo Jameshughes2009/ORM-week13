9db9e1a4e4070e3b299460edf22980ed2be13f87
James Hughes <jameshughes2009@hotmail.com>
2024-04-07 10:10:43 -0400

    more testing

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -35,13 +35,13 @@ router.post('/', async (req, res) => {
     const newCategory = await Category.create(req.body);
     res.status(200).json(newCategory);
   } catch (err){
-    res.status(400).json
+    res.status(400).json;
   }
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
-});
+// router.put('/:id', (req, res) => {
+//   // update a category by its `id` value
+// });
 
 // router.delete('/:id', (req, res) => {
 //   // delete a category by its `id` value
@@ -51,5 +51,15 @@ router.put('/:id', (req, res) => {
       
 //   }
 // });
-
+router.put('/:id', async (req, res) => {
+  try {
+    const updated = await Category.update(req.body, { 
+      where: {id: req.params.id}, });
+    !updated[0]
+      ? res.status(404).json({message: "Cannot find tag with id!"})
+      : res.status(200).json(updated);
+  } catch (err) {
+    res.status(500).json({message:"Failed to Upadte"})
+  }
+});
 module.exports = router;
